6fa9939bcabba6959cbc872d21f0d548d14477c2
arensin8 <arensinani4@gmail.com>
2024-07-17 14:09:44 +0400

    download link generator bot

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
+require("dotenv").config();
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.on(message("document"), async (ctx) => {
+  try {
+    ctx.sendChatAction("upload_document");
+    const docId = ctx.message.document.file_id;
+    const link = await bot.telegram.getFileLink(docId);
+    ctx.reply("Yor download link is:" + link, {
+      reply_to_message_id: ctx.message.message_id,
+    });
+  } catch (error) {
+    ctx.reply(error?.message ?? error.description);
+  }
+});
+
+bot.on(message("photo"), async (ctx) => {
+  try {
+    ctx.sendChatAction("upload_photo");
+    const photoId = ctx.message.photo[0].file_id;
+    const link = await bot.telegram.getFileLink(photoId);
+    ctx.reply("Yor download link is:" + link, {
+      reply_to_message_id: ctx.message.message_id,
+    });
+  } catch (error) {
+    ctx.reply(error?.message ?? error.description);
+  }
+});
+
+bot.launch();
